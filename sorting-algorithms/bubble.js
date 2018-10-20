363be4d951f800843c5b3b3711d3c0b0cd18082e
Evgeniy Bulgakov <zoom81@yandex.ru>
2018-10-20 22:06:53 +0400

    Bubble sorting

--- sorting-algorithms/bubble.js
+++ sorting-algorithms/bubble.js
@@ -19,3 +19,26 @@ Variants:
 (https://en.wikipedia.org/wiki/Cocktail_sort)
 
 */
+
+function bubbleSorting (input) {
+  const start = window.performance.now();
+  const arr = input.slice(0);
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+
+      if(arr[j] > arr[j+1]) {
+        const temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+
+  const finish = window.performance.now();
+
+  return {
+    time: `${finish - start} ms`,
+    result: arr
+  }
+}
