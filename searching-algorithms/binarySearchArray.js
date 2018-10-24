b6387be368f93032190a46d7e90fc146a494c57e
Evgeniy Bulgakov <zoom81@yandex.ru>
2018-10-24 16:02:46 +0400

    Binary search interactive

--- searching-algorithms/binarySearchArray.js
+++ searching-algorithms/binarySearchArray.js
@@ -12,3 +12,25 @@ Write a function that takes a sorted array and a value and returns the index of 
 Extra credit: Implement the function both iteratively and recursively.
 
 */
+
+function binarySearchInteractive(list, item) {
+  let min = 0;
+  let max = list.length - 1;
+  let mid;
+
+  while (min <= max) {
+    mid = Math.floor((min + max) / 2); // To separate array at the middle. => Index of array
+
+    if (list[mid] === item) {
+      return mid;
+    } else {
+      if (item > list[mid]) {
+        min = mid + 1;
+      } else {
+        max = mid - 1;
+      }
+    }
+  }
+
+  return -1;
+}
